f75c59f37f40e4770a4990b56778cbd3bf35756c
Christopher Wiggins <cwiggins610@gmail.com>
2023-09-29 09:21:14 -0400

    passing

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Keypad from "keypad";
+
+function App() {
+  return (
+    <div className="App">
+      <Keypad />
+    </div>
+  );
+}
+
+export default App;
--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
-// Code EyesOnMe Component Here
+import React, { useState } from "react";
+
+function EyesOnMe() {
+  const [Focused, setFocused] = useState(false);
+
+  const Focus = () => {
+    console.log("Good!");
+    setFocused(true);
+  };
+
+  const Blur = () => {
+    console.log("Hey! Eyes on me!");
+    setFocused(false);
+  };
+
+  return (
+    <button onFocus={Focus} onBlur={Blur}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
